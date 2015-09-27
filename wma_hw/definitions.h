3cf4d2f3fbf8d51367f1fae67cec794ebe133d84
johnny <johnnywang@gmail.com>
2015-09-27 17:14:07 -0400

    Fixed bug in Adversarial Nature. Also added definition file.

---
+++ wma_hw/definitions.h
@@ -0,0 +1,8 @@
+
+#define WMA  0
+#define RWMA 1
+
+// Nature types
+enum NAT_TYPE { DETER, STOCH, ADVER };
+// Expert types
+enum EXP_TYPE { DIEHARD, PESSIMIST, ODDEVEN, WEATHER, FIELD, LOCATION, OPPWIN };
